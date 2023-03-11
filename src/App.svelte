ff9eb7f0b890c16dbfef316d07a1b4ca5e7e4884
vanillovin <vanillovin@gmail.com>
2023-03-11 16:26:29 +0900

    조건문과 반복문

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	let name = 'Foods';
+	let foods = ['떡볶이', '마카롱', '초밥', '샐러드', '피자', '치킨'];
+	let toggle = false;
+	function changeToggle() {
+		toggle = !toggle;
+	}
+	function deleteFood(food) {
+		alert(`${food} 냠냠!`);
+		foods = foods.filter((oldFood) => oldFood !== food);
+	}
+</script>
+
+<button on:click={changeToggle}>Toggle {toggle ? 'off' : 'on'}</button>
+{#if toggle}
+	<h1 class={toggle ? 'active' : ''}>Hello {name}!</h1>
+	<input type="txt" bind:value={name} />
+{:else}
+	<div>No name!</div>
+{/if}
+
+<ul>
+	{#each foods as food}
+		<li>
+			{food}
+			<button on:click={() => deleteFood(food)}>Eat it!</button>
+		</li>
+	{/each}
+</ul>
+
+<style>
+	.active {
+		color: white;
+		background-color: royalblue;
+	}
+</style>
