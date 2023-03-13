bf9afa43252ebcd94d8fe53162df33c2aaca6dd3
vanillovin <vanillovin@gmail.com>
2023-03-13 22:36:44 +0900

    클래스와 스타일 속성 바인딩

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,47 @@
 <script lang="ts">
-	import TodoForm from './components/TodoForm.svelte';
-	import TodoList from './components/TodoList.svelte';
+	let active = false;
+	const style = {
+		r: 'rosybrown',
+		w: '#fff',
+		letterSpacing: 'letter-spacing: .25rem'
+	};
 </script>
 
-<div class="todoContainer">
-	<TodoForm />
-	<TodoList />
-</div>
+<section>
+	<h1 style="background-color: {style.r}; color: {style.w}; {style.letterSpacing}">Hello</h1>
+	<!-- class directive(지시어). 마치 클래스 속성처럼 작성 -->
+	<!-- active 라는 클래스 이름은 어떠한 데이터에 의해 붙거나 붙지 않음 -->
+	<div class:active>Hello</div>
+	<button
+		on:click={() => {
+			active = !active;
+		}}>Toggle!</button
+	>
+</section>
 
 <style>
+	div {
+		color: beige;
+		width: 18.75rem;
+		height: 18.75rem;
+		background: cornflowerblue;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 0.625rem;
+	}
+	button {
+		padding: 0.25rem;
+		margin: 0.25rem;
+		border: 0.125rem solid black;
+		font-weight: 600;
+	}
+	button:hover {
+		color: white;
+		background-color: black;
+	}
+	.active {
+		color: black;
+		background: cadetblue;
+	}
 </style>
